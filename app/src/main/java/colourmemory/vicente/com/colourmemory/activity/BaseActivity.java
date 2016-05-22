3704f713b0c005ec31a350b415bc6bc69c6294cb
vicente angcaway <vinzdez@gmail.com>
2016-05-22 23:14:30 +0800

    add back on score list

--- app/src/main/java/colourmemory/vicente/com/colourmemory/activity/BaseActivity.java
+++ app/src/main/java/colourmemory/vicente/com/colourmemory/activity/BaseActivity.java
@@ -2,10 +2,6 @@ package colourmemory.vicente.com.colourmemory.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import java.util.ArrayList;
 
 import colourmemory.vicente.com.colourmemory.service.ScoreService;
 import colourmemory.vicente.com.colourmemory.util.ActivityUtils;
@@ -30,27 +26,6 @@ public class BaseActivity extends AppCompatActivity implements Navigator {
         ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), colourHighScoreFragment, tag);
 
         HighScorePresenter highScorePresenter = new HighScorePresenter(colourHighScoreFragment, new ScoreService(this));
-    }
-
-    public static View getToolbarLogoIcon(Toolbar toolbar) {
-        //check if contentDescription previously was set
-        boolean hadContentDescription = android.text.TextUtils.isEmpty(toolbar.getLogoDescription());
-        String contentDescription = String.valueOf(!hadContentDescription ? toolbar.getLogoDescription() : "logoContentDescription");
-        toolbar.setLogoDescription(contentDescription);
-        ArrayList<View> potentialViews = new ArrayList<>();
-
-        //find the view based on it's content description, set programatically or with android:contentDescription
-        toolbar.findViewsWithText(potentialViews, contentDescription, View.FIND_VIEWS_WITH_CONTENT_DESCRIPTION);
-
-        //Nav icon is always instantiated at this point because calling setLogoDescription ensures its existence
-        View logoIcon = null;
-        if (potentialViews.size() > 0) {
-            logoIcon = potentialViews.get(0);
-        }
-        //Clear content description if not previously present
-        if (hadContentDescription)
-            toolbar.setLogoDescription(null);
-        return logoIcon;
     }
 
     public void showColourMemoryFragment() {
--- app/src/main/java/colourmemory/vicente/com/colourmemory/activity/ColourMemoryActivity.java
+++ app/src/main/java/colourmemory/vicente/com/colourmemory/activity/ColourMemoryActivity.java
@@ -2,15 +2,15 @@ package colourmemory.vicente.com.colourmemory.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import colourmemory.vicente.com.colourmemory.R;
 
 
-public class ColourMemoryActivity extends BaseActivity {
+public class ColourMemoryActivity extends BaseActivity implements FragmentManager.OnBackStackChangedListener {
 
     private Toolbar toolbar;
 
@@ -19,22 +19,24 @@ public class ColourMemoryActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colour_memory);
         initToolBar();
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
         showColourMemoryFragment();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch(item.getItemId()){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.id_menu_high_score:
                 showHighScore();
                 return true;
-            default: return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
@@ -52,18 +54,36 @@ public class ColourMemoryActivity extends BaseActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
-      /*  toolbar.setLogo(R.drawable.logo);
-        View logoView = getToolbarLogoIcon(toolbar);
-        logoView.setOnClickListener(new LocgoClickListener());*/
         setSupportActionBar(toolbar);
+
     }
 
-    public class LocgoClickListener implements View.OnClickListener {
+    @Override
+    public void onBackStackChanged() {
+        shouldDisplayHomeUp();
+    }
 
-        @Override
-        public void onClick(View v) {
-            showColourMemoryFragment();
+    @Override
+    public boolean onSupportNavigateUp() {
+        //This method is called when the up button is pressed. Just the pop back stack.
+        getSupportFragmentManager().popBackStack();
+        showColourMemoryFragment();
+        shouldDisplayHomeUp();
+        return true;
+    }
+
+    public void shouldDisplayHomeUp() {
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            if ((getSupportActionBar() != null && fragment != null) && fragment.isVisible()) {
+                if (ColourHighScoreFragment.COLOUR_HIGH_SCORE_FRAGMENT == fragment.getTag()) {
+                    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                } else if (ColourMemoryFragment.COLOUR_MEMORY_FRAGMENT == fragment.getTag()) {
+                    getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                }
+            }
+
         }
     }
+
 
 }
