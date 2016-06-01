f5bfd1473c0b32a501d2d7b94913874d3d1f3692
vicente angcaway <vinzdez@gmail.com>
2016-06-01 14:02:49 +0800

    Reshuffle cards when user swipe's down from the screen

--- app/src/main/java/colormemory/vicente/com/colormemory/activity/ColorMemoryFragment.java
+++ app/src/main/java/colormemory/vicente/com/colormemory/activity/ColorMemoryFragment.java
@@ -16,7 +16,6 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import colormemory.vicente.com.colormemory.R;
@@ -33,114 +32,115 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class ColorMemoryFragment extends Fragment implements CardContract.View {
 
-    public static final String COLOR_MEMORY_FRAGMENT = "colourmemory.vicente.com.colourmemory.activity.ColorMemoryFragment";
+  public static final String COLOR_MEMORY_FRAGMENT =
+      "colourmemory.vicente.com.colourmemory.activity.ColorMemoryFragment";
 
-    public static ColorMemoryFragment newInstance() {
-        return new ColorMemoryFragment();
+  public static ColorMemoryFragment newInstance() {
+    return new ColorMemoryFragment();
+  }
+
+  private Context context;
+
+  @BindView(R.id.gridview)
+  GridView gridView;
+
+  private SwipeRefreshLayout colorFragView;
+  private Navigator navigator;
+
+  private CardAdapter cardAdapter;
+  private CardContract.Presenter cardPresenter;
+
+  @NonNull
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    if (colorFragView == null) {
+      this.context = getActivity();
+      this.colorFragView = (SwipeRefreshLayout) inflater.inflate(R.layout.fragment_color, container, false);
+      ButterKnife.bind(this, colorFragView);
+      colorFragView.setOnRefreshListener(this);
+      colorFragView.setColorSchemeColors(Color.GRAY, Color.BLACK, Color.BLUE, Color.RED);
+      this.cardAdapter = new CardAdapter(context, this);
     }
+    reshuffleCards();
+    gridView.setAdapter(cardAdapter);
+    return colorFragView;
+  }
 
-    private Context context;
+  @Override
+  public void updateScoreBoard(String score) {
+    String value = getString(R.string.score_label).concat(": ").concat(score);
 
-    @BindView(R.id.gridview)
-    GridView gridView;
+    //TODO:update Activity
+    // textScore.setText(value);
+  }
 
-    private SwipeRefreshLayout colorFragView;
-    private Navigator navigator;
+  @Override
+  public void showUserInputDialog(int score) {
+    LayoutInflater layoutInflaterAndroid = LayoutInflater.from(context);
+    View dialogView = layoutInflaterAndroid.inflate(R.layout.user_input_dialog_box, null);
+    TextView dialogTitle = (TextView) dialogView.findViewById(R.id.dialogTitle);
+    dialogTitle.setText(dialogTitle.getText().toString().concat(" ").concat(String.valueOf(score)));
+    EditText userInputDialogEditText = (EditText) dialogView.findViewById(R.id.userInputDialog);
+    showAlertDialog(AlertManager.getAlertDialogBuilder(context, dialogView), userInputDialogEditText, score);
+  }
 
-    private CardAdapter cardAdapter;
-    private CardContract.Presenter cardPresenter;
+  @Override
+  public void showProgressBar() {
+    Toast.makeText(getActivity(), getString(R.string.progress_message), Toast.LENGTH_LONG).show();
+    colorFragView.setRefreshing(true);
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        colorFragView.setRefreshing(false);
+      }
+    }, 5000);
+  }
 
-    @NonNull
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (colorFragView == null) {
-            this.context = getActivity();
-            this.colorFragView = (SwipeRefreshLayout) inflater.inflate(R.layout.fragment_color, container, false);
-            ButterKnife.bind(this, colorFragView);
-            colorFragView.setOnRefreshListener(this);
-            colorFragView.setColorSchemeColors(Color.GRAY, Color.BLACK, Color.BLUE, Color.RED);
-            this.cardAdapter = new CardAdapter(context, this);
+  @Override
+  public void setPresenter(@NonNull CardContract.Presenter presenter) {
+    cardPresenter = checkNotNull(presenter);
+  }
+
+  private void showAlertDialog(AlertDialog.Builder alertDialogBuilderUserInput, final EditText userInputDialogEditText,
+      final int score) {
+    AlertManager.showDialog(alertDialogBuilderUserInput, new AlertManager.ButtonPositiveCallback() {
+      @Override
+      public void onSelect(AlertDialog dialog) {
+        if (TextUtils.isEmpty(userInputDialogEditText.getText())) {
+          userInputDialogEditText.setError(getString(R.string.user_name_warning));
+        } else {
+          cardPresenter.saveScore(new ScoreViewModel(userInputDialogEditText.getText().toString(), score));
+          userInputDialogEditText.setError(null);
+          getNavigator().showHighScore();
+          dialog.dismiss();
         }
-        reshuffleCards();
-        gridView.setAdapter(cardAdapter);
-        return colorFragView;
-    }
+      }
+    });
+  }
 
-    @Override
-    public void updateScoreBoard(String score) {
-        String value = getString(R.string.score_label).concat(": ").concat(score);
+  private void reshuffleCards() {
+    cardPresenter.shuffleCards();
+    cardAdapter.setCards(cardPresenter.getCards());
+  }
 
-        //TODO:update Activity
-        // textScore.setText(value);
-    }
+  public Navigator getNavigator() {
+    return navigator;
+  }
 
-    @Override
-    public void showUserInputDialog(int score) {
-        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(context);
-        View dialogView = layoutInflaterAndroid.inflate(R.layout.user_input_dialog_box, null);
-        TextView dialogTitle = (TextView) dialogView.findViewById(R.id.dialogTitle);
-        dialogTitle.setText(dialogTitle.getText().toString().concat(" ").concat(String.valueOf(score)));
-        EditText userInputDialogEditText = (EditText) dialogView.findViewById(R.id.userInputDialog);
-        showAlertDialog(AlertManager.getAlertDialogBuilder(context, dialogView), userInputDialogEditText, score);
-    }
+  public void setNavigator(Navigator navigator) {
+    this.navigator = navigator;
+  }
 
-    @Override
-    public void showProgressBar() {
-        colorFragView.setRefreshing(true);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getActivity(), getString(R.string.progress_message), Toast.LENGTH_LONG).show();
-                colorFragView.setRefreshing(false);
-            }
-        }, 5000);
-    }
-
-    @Override
-    public void setPresenter(@NonNull CardContract.Presenter presenter) {
-        cardPresenter = checkNotNull(presenter);
-    }
-
-    private void showAlertDialog(AlertDialog.Builder alertDialogBuilderUserInput, final EditText userInputDialogEditText, final int score) {
-        AlertManager.showDialog(alertDialogBuilderUserInput, new AlertManager.ButtonPositiveCallback() {
-            @Override
-            public void onSelect(AlertDialog dialog) {
-                if (TextUtils.isEmpty(userInputDialogEditText.getText())) {
-                    userInputDialogEditText.setError(getString(R.string.user_name_warning));
-                } else {
-                    cardPresenter.saveScore(new ScoreViewModel(userInputDialogEditText.getText().toString(), score));
-                    userInputDialogEditText.setError(null);
-                    getNavigator().showHighScore();
-                    dialog.dismiss();
-                }
-            }
-        });
-    }
-
-    private void reshuffleCards() {
-        cardPresenter.shuffleCards();
-        cardAdapter.setCards(cardPresenter.getCards());
-    }
-
-    public Navigator getNavigator() {
-        return navigator;
-    }
-
-    public void setNavigator(Navigator navigator) {
-        this.navigator = navigator;
-    }
-
-    @Override
-    public void onRefresh() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //TODO: reload data
-                Toast.makeText(getActivity(), "On Refresh Happend", Toast.LENGTH_LONG).show();
-                colorFragView.setRefreshing(false);
-            }
-        }, 5000);
-    }
-
-
+  @Override
+  public void onRefresh() {
+    reshuffleCards();
+    cardAdapter.setSwipe(true);
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        cardAdapter.notifyDataSetChanged();
+        colorFragView.setRefreshing(false);
+      }
+    }, 5000);
+  }
 }
--- app/src/main/java/colormemory/vicente/com/colormemory/adapter/CardAdapter.java
+++ app/src/main/java/colormemory/vicente/com/colormemory/adapter/CardAdapter.java
@@ -10,270 +10,270 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import colormemory.vicente.com.colormemory.R;
 import colormemory.vicente.com.colormemory.model.Card;
 import colormemory.vicente.com.colormemory.view.CardContract;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Vicente on 4/23/2016.
  */
 public class CardAdapter extends BaseAdapter {
 
+  private ViewHolder holder;
+  private Map<Integer, Card> cardMap;
+  private Map<Integer, Integer> cards;
+  private Context context;
 
-    private ViewHolder holder;
-    private Map<Integer, Card> cardMap;
-    private Map<Integer, Integer> cards;
-    private Context context;
+  private int gameSpan;
+  private int score;
 
-    private int gameSpan;
-    private int score;
+  private boolean isSwipe;
 
-    private Animation frontAnimation;
-    private Animation backAnimation;
+  private Animation frontAnimation;
+  private Animation backAnimation;
 
+  private CardContract.View cardViewAction;
 
-    private CardContract.View cardViewAction;
+  private boolean allowFutureClick;
 
-    private boolean allowFutureClick;
+  public CardAdapter(Context context, CardContract.View cardViewAction) {
+    this.cardMap = new HashMap<>();
+    this.context = context;
+    this.allowFutureClick = true;
+    this.cardViewAction = cardViewAction;
+    this.frontAnimation = AnimationUtils.loadAnimation(context, R.anim.to_left);
+    this.backAnimation = AnimationUtils.loadAnimation(context, R.anim.to_right);
+  }
 
-    public CardAdapter(Context context, CardContract.View cardViewAction) {
-        this.cardMap = new HashMap<>();
-        this.context = context;
-        this.allowFutureClick = true;
-        this.cardViewAction = cardViewAction;
-        this.frontAnimation = AnimationUtils.loadAnimation(context, R.anim.to_left);
-        this.backAnimation = AnimationUtils.loadAnimation(context, R.anim.to_right);
+  @Override
+  public int getCount() {
+    return getCards().size();
+  }
 
+  @Override
+  public Card getItem(int position) {
+    return cardMap.get(position);
+  }
 
+  @Override
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  @Override
+  public View getView(final int position, View convertView, ViewGroup parent) {
+
+    if (convertView == null) {
+      convertView = ((Activity) context).getLayoutInflater().inflate(R.layout.card_layout, parent, false);
+      holder = new ViewHolder(convertView);
+      convertView.setTag(holder);
+    } else {
+      holder = (ViewHolder) convertView.getTag();
     }
 
-    @Override
-    public int getCount() {
-        return getCards().size();
+    if (cardMap.get(position) == null) {
+      holder.image.setEnabled(false);
+      holder.image.setOnClickListener(new ImageViewListener(position));
+      cardMap.put(position, new Card(holder.image, position));
+      showImageView(holder.image, position);
+    } else if (cardMap.size() == getCount()) {
+      showImageView(holder.image, position);
     }
 
-    @Override
-    public Card getItem(int position) {
-        return cardMap.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-
-        if (convertView == null) {
-            convertView = ((Activity) context).getLayoutInflater().inflate(R.layout.card_layout, parent, false);
-            holder = new ViewHolder(convertView);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        if (cardMap.get(position) == null) {
-            holder.image.setEnabled(false);
-            holder.image.setOnClickListener(new ImageViewListener(position));
-            cardMap.put(position, new Card(holder.image, position));
-            showImageView(holder.image, position);
-        }
-
-        if (position == (getCount() - 1)) {
-            cardViewAction.showProgressBar();
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    for (int key : cardMap.keySet()) {
-                        ImageView imageView = cardMap.get(key).getImageView();
-                        imageView.setEnabled(true);
-                        showDeafaultImage(imageView);
-                    }
-                }
-            }, 5000);
-
-        }
-        return convertView;
-    }
-
-
-    public Map<Integer, Integer> getCards() {
-        return cards;
-    }
-
-    public void setCards(Map<Integer, Integer> cards) {
-        this.cards = cards;
-    }
-
-    public class ImageViewListener implements View.OnClickListener, Animation.AnimationListener, Runnable {
-
-        private int currentPosition;
-
-
-        private Card currentCard;
-        private Card previousCard;
-
-        public ImageViewListener(int currentPosition) {
-            this.currentPosition = currentPosition;
-        }
-
-        @Override
-        public void onClick(View v) {
-            frontAnimation.setAnimationListener(this);
-            backAnimation.setAnimationListener(this);
-            handleImageClick();
-        }
-
-        /**
-         * Check Previous and Current Card Equality
-         * Update Score Bored
-         * Calls a callback to HighScore if game has ended
-         */
+    if (position == (getCount() - 1)) {
+      if (!isSwipe()) {
+        cardViewAction.showProgressBar();
+      }
+      Handler handler = new Handler();
+      handler.postDelayed(new Runnable() {
         @Override
         public void run() {
-            checkCardEquality();
-            cardViewAction.updateScoreBoard(String.valueOf(score));
-            if (isGameEnded()) {
-                cardViewAction.showUserInputDialog(score);
-            } else {
-                allowFutureClick = true;
-            }
+          for (int key : cardMap.keySet()) {
+            ImageView imageView = cardMap.get(key).getImageView();
+            imageView.setEnabled(true);
+            showDeafaultImage(imageView);
+          }
         }
+      }, 5000);
+    }
+    return convertView;
+  }
 
-        @Override
-        public void onAnimationStart(Animation animation) {
-            if (animation == frontAnimation) {
+  public Map<Integer, Integer> getCards() {
+    return cards;
+  }
 
-                if (currentCard.isCardInitialyClick() && allowFutureClick) {
-                    //shows the first card
-                    showImageView(currentCard.getImageView(), currentPosition);
-                } else if (!currentCard.isCardInitialyClick() && !allowFutureClick) {
-                    //shows the second card
-                    showImageView(currentCard.getImageView(), currentPosition);
-                } else if (!previousCard.isVisible()) {
-                    //if both card are not equal
-                    showDeafaultImage(previousCard.getImageView());
-                    showDeafaultImage(currentCard.getImageView());
-                    previousCard.getImageView().clearAnimation();
-                    previousCard.getImageView().setAnimation(backAnimation);
-                    previousCard.getImageView().startAnimation(backAnimation);
-                }
-                currentCard.getImageView().clearAnimation();
-                currentCard.getImageView().setAnimation(backAnimation);
-                currentCard.getImageView().startAnimation(backAnimation);
+  public void setCards(Map<Integer, Integer> cards) {
+    this.cards = cards;
+  }
 
-            }
-        }
+  public class ImageViewListener implements View.OnClickListener, Animation.AnimationListener, Runnable {
 
-        @Override
-        public void onAnimationEnd(Animation animation) {
-        }
+    private int currentPosition;
 
-        @Override
-        public void onAnimationRepeat(Animation animation) {
-        }
+    private Card currentCard;
+    private Card previousCard;
 
-        /**
-         * Calculate's card to show or not
-         */
-        public void handleImageClick() {
-            this.currentCard = getItem(currentPosition);
-
-            //stop user to click initial card, and while evaluating card equality
-            if (!currentCard.isCardInitialyClick() && allowFutureClick) {
-                ImageView currentCardImage = currentCard.getImageView();
-                animateCard(currentCardImage);
-
-                for (int i = 0; i <= getCount(); i++) {
-
-                    //if i reached max count, set card as visible, current card is first click
-                    if (i == getCount()) {
-                        currentCard.setVisible(true);
-                        break;
-                    }
-
-                    this.previousCard = getItem(i);
-                    if (previousCard.isCardInitialyClick()) {
-                        allowFutureClick = false;
-                        currentCardImage.postDelayed(this, 1000);
-                        break;
-                    }
-                }
-            }
-        }
-
-        private boolean isGameEnded() {
-            return (gameSpan * 2) == getCount();
-        }
-
-        private void checkCardEquality() {
-            if (getCards().get(previousCard.getPosition()).equals(getCards().get(currentPosition))) {
-                //Disable card
-                previousCard.setDisabled(true);
-                currentCard.setDisabled(true);
-
-                //Disable card
-                previousCard.getImageView().setClickable(false);
-                currentCard.getImageView().setClickable(false);
-
-                //Add 2 points
-                score += 2;
-                gameSpan++;
-            } else {
-                //Card no longer the firs card click
-                currentCard.setVisible(false);
-                previousCard.setVisible(false);
-
-                animateCard(previousCard.getImageView());
-                //Minus 1 point
-                score -= 1;
-            }
-        }
-
-        /**
-         * Start Animation
-         *
-         * @param imageView
-         */
-        private void animateCard(ImageView imageView) {
-            imageView.clearAnimation();
-            imageView.setAnimation(frontAnimation);
-            imageView.startAnimation(frontAnimation);
-        }
-
-
+    public ImageViewListener(int currentPosition) {
+      this.currentPosition = currentPosition;
     }
 
-    private void showDeafaultImage(ImageView imageView) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            imageView.setImageDrawable(context.getDrawable(R.drawable.card_bg));
-        } else {
-            imageView.setImageDrawable(context.getResources().getDrawable(R.drawable.card_bg));
-        }
+    @Override
+    public void onClick(View v) {
+      frontAnimation.setAnimationListener(this);
+      backAnimation.setAnimationListener(this);
+      handleImageClick();
     }
 
-    private void showImageView(ImageView imageView, int index) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            imageView.setImageDrawable(context.getDrawable(getCards().get(index)));
-        } else {
-            imageView.setImageDrawable(context.getResources().getDrawable(getCards().get(index)));
-        }
+    /**
+     * Check Previous and Current Card Equality
+     * Update Score Bored
+     * Calls a callback to HighScore if game has ended
+     */
+    @Override
+    public void run() {
+      checkCardEquality();
+      cardViewAction.updateScoreBoard(String.valueOf(score));
+      if (isGameEnded()) {
+        cardViewAction.showUserInputDialog(score);
+      } else {
+        allowFutureClick = true;
+      }
     }
 
-    class ViewHolder {
-        @BindView(R.id.image)
-        ImageView image;
+    @Override
+    public void onAnimationStart(Animation animation) {
+      if (animation == frontAnimation) {
 
-        public ViewHolder(View itemView) {
-            ButterKnife.bind(this, itemView);
+        if (currentCard.isCardInitialyClick() && allowFutureClick) {
+          //shows the first card
+          showImageView(currentCard.getImageView(), currentPosition);
+        } else if (!currentCard.isCardInitialyClick() && !allowFutureClick) {
+          //shows the second card
+          showImageView(currentCard.getImageView(), currentPosition);
+        } else if (!previousCard.isVisible()) {
+          //if both card are not equal
+          showDeafaultImage(previousCard.getImageView());
+          showDeafaultImage(currentCard.getImageView());
+          previousCard.getImageView().clearAnimation();
+          previousCard.getImageView().setAnimation(backAnimation);
+          previousCard.getImageView().startAnimation(backAnimation);
         }
+        currentCard.getImageView().clearAnimation();
+        currentCard.getImageView().setAnimation(backAnimation);
+        currentCard.getImageView().startAnimation(backAnimation);
+      }
     }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+
+    /**
+     * Calculate's card to show or not
+     */
+    public void handleImageClick() {
+      this.currentCard = getItem(currentPosition);
+
+      //stop user to click initial card, and while evaluating card equality
+      if (!currentCard.isCardInitialyClick() && allowFutureClick) {
+        ImageView currentCardImage = currentCard.getImageView();
+        animateCard(currentCardImage);
+
+        for (int i = 0; i <= getCount(); i++) {
+
+          //if i reached max count, set card as visible, current card is first click
+          if (i == getCount()) {
+            currentCard.setVisible(true);
+            break;
+          }
+
+          this.previousCard = getItem(i);
+          if (previousCard.isCardInitialyClick()) {
+            allowFutureClick = false;
+            currentCardImage.postDelayed(this, 1000);
+            break;
+          }
+        }
+      }
+    }
+
+    private boolean isGameEnded() {
+      return (gameSpan * 2) == getCount();
+    }
+
+    private void checkCardEquality() {
+      if (getCards().get(previousCard.getPosition()).equals(getCards().get(currentPosition))) {
+        //Disable card
+        previousCard.setDisabled(true);
+        currentCard.setDisabled(true);
+
+        //Disable card
+        previousCard.getImageView().setClickable(false);
+        currentCard.getImageView().setClickable(false);
+
+        //Add 2 points
+        score += 2;
+        gameSpan++;
+      } else {
+        //Card no longer the firs card click
+        currentCard.setVisible(false);
+        previousCard.setVisible(false);
+
+        animateCard(previousCard.getImageView());
+        //Minus 1 point
+        score -= 1;
+      }
+    }
+
+    /**
+     * Start Animation
+     */
+    private void animateCard(ImageView imageView) {
+      imageView.clearAnimation();
+      imageView.setAnimation(frontAnimation);
+      imageView.startAnimation(frontAnimation);
+    }
+  }
+
+  private void showDeafaultImage(ImageView imageView) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      imageView.setImageDrawable(context.getDrawable(R.drawable.card_bg));
+    } else {
+      imageView.setImageDrawable(context.getResources().getDrawable(R.drawable.card_bg));
+    }
+  }
+
+  private void showImageView(ImageView imageView, int index) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      imageView.setImageDrawable(context.getDrawable(getCards().get(index)));
+    } else {
+      imageView.setImageDrawable(context.getResources().getDrawable(getCards().get(index)));
+    }
+  }
+
+  public boolean isSwipe() {
+    return isSwipe;
+  }
+
+  public void setSwipe(boolean swipe) {
+    isSwipe = swipe;
+  }
+
+  class ViewHolder {
+    @BindView(R.id.image)
+    ImageView image;
+
+    public ViewHolder(View itemView) {
+      ButterKnife.bind(this, itemView);
+    }
+  }
 }
