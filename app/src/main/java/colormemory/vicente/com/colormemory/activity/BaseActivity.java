f44abe1b40ed0ba436bd8726735a48b9ffd49645
vicente angcaway <vinzdez@gmail.com>
2016-06-07 22:56:25 +0800

    initial Comit for ShowCase

--- app/src/main/java/colormemory/vicente/com/colormemory/activity/BaseActivity.java
+++ app/src/main/java/colormemory/vicente/com/colormemory/activity/BaseActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import colormemory.vicente.com.colormemory.R;
 import colormemory.vicente.com.colormemory.view.CardContract;
@@ -27,8 +28,18 @@ public abstract class BaseActivity extends AppCompatActivity implements Navigato
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        View view = findViewById(R.id.id_menu_high_score);
         switch (item.getItemId()) {
             case R.id.id_menu_high_score:
+                //TODO : add preferences if ShowCase appeared
+                /*new MaterialShowcaseView.Builder(this)
+                        .setTarget(view)
+                        .setShapePadding(96)
+                        .setDismissText("GOT IT")
+                        .setContentText("Show Score Results.")
+                        .setContentTextColor(getResources().getColor(R.color.cardview_light_background))
+                        .setMaskColour(getResources().getColor(R.color.cardview_dark_background))
+                        .show();*/
                 showHighScore();
                 return true;
             default:
