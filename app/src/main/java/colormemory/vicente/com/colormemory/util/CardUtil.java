51258aa5807e9bdb518afc850454f3bb3dac6e99
vicente angcaway <vinzdez@gmail.com>
2016-06-21 23:17:10 +0800

    Update images

--- app/src/main/java/colormemory/vicente/com/colormemory/util/CardUtil.java
+++ app/src/main/java/colormemory/vicente/com/colormemory/util/CardUtil.java
@@ -1,34 +1,35 @@
 package colormemory.vicente.com.colormemory.util;
 
-import colormemory.vicente.com.colormemory.R;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import colormemory.vicente.com.colormemory.R;
+
 /**
  * Created by Vicente on 4/21/2016.
  */
 public class CardUtil {
 
-  private static final int[] cards = new int[] {
-      R.drawable.color1, R.drawable.color2, R.drawable.color3, R.drawable.color4, R.drawable.color5, R.drawable.color6,
-      R.drawable.color7, R.drawable.color8, R.drawable.color1, R.drawable.color2, R.drawable.color3, R.drawable.color4,
-      R.drawable.color5, R.drawable.color6, R.drawable.color7, R.drawable.color8
-  };
+    private static final int[] cards = new int[]{
+            R.drawable.card1, R.drawable.card2, R.drawable.card3, R.drawable.card4, R.drawable.card5, R.drawable.card6,
+            R.drawable.card7, R.drawable.card8, R.drawable.card1, R.drawable.card2, R.drawable.card3, R.drawable.card4, R.drawable.card5, R.drawable.card6,
+            R.drawable.card7, R.drawable.card8
+    };
 
-  public static Map<Integer, Integer> shuffleCards() {
-    List<Integer> myArray = new ArrayList<>();
-    Map<Integer, Integer> cardImageMap = new HashMap<>();
-    for (int card : cards) {
-      myArray.add(card);
-    }
-    Collections.shuffle(myArray);
+    public static Map<Integer, Integer> shuffleCards() {
+        List<Integer> myArray = new ArrayList<>();
+        Map<Integer, Integer> cardImageMap = new HashMap<>();
+        for (int card : cards) {
+            myArray.add(card);
+        }
+        Collections.shuffle(myArray);
 
-    for (int x = 0; x < cards.length; x++) {
-      cardImageMap.put(x, myArray.get(x));
+        for (int x = 0; x < cards.length; x++) {
+            cardImageMap.put(x, myArray.get(x));
+        }
+        return cardImageMap;
     }
-    return cardImageMap;
-  }
 }
